e0477a9f0dfd113b9d0c2a0437e378973434d59c
broroot <168093278+broroot@users.noreply.github.com>
2025-07-21 21:30:57 +0900

    02 데이터 타입
    - 문자열 / 숫자 / 불리안 자료형
    - 띄어쓰기는 그냥 보기 편하라고

---
+++ _02_DataTypes.java
@@ -0,0 +1,24 @@
+package Chap_01;
+
+/* public static void main(String[] args) 불러오기
+1. psvm (public static void main)
+2. main
+ */
+
+/* System.out.println(); 불러오기
+1. sout
+ */
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("eds"); //문자열
+        System.out.println("하이루");
+        System.out.println(12); //숫자(열)
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true); //불리언(안) 자료형
+        System.out.println(false);
+
+        System.out.println(123 + 234); //????띄어쓰기 왜???? 아 보기 편하라고ㅇㅇ
+    }
+}
