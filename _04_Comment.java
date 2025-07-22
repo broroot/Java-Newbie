70aa7baf68645d4c6ec1a3e997f592ce1011102b
broroot <168093278+broroot@users.noreply.github.com>
2025-07-22 22:08:24 +0900

    04 주석
    Ctrl + D : 문장 복사
    Ctrl + / : 한줄 주석 처리 (//)
    Ctrl + Shift + / : 여러줄 주석 처리 (/**/)

---
+++ _04_Comment.java
@@ -0,0 +1,21 @@
+package Chap_01;
+
+public class _04_Comment {
+    public static void main(String[] args) {
+        System.out.println("(10분 전) 잠시 후 결혼식 시작 예정이오니 착석 부탁드립니다.");     // ctrl+D : 문장복사
+        //System.out.println("(5분 전) 잠시 후 결혼식 시작 예정이오니 착석 부탁드립니다.");
+        System.out.println("지금부터 결혼식을 시작하겠습니다.");
+
+        int size = 120;
+        size += 10;           // 어린이는 빨리 커
+        System.out.println("신발 사이즈 " + size + "으로 보여주세요.");
+
+        /* int a = 10;
+        int b = 20;
+        System.out.println(a+b);    //30
+         */
+
+                                                  // Ctrl + / : 선택 한 줄 주석 처리/해제 (//)
+                                                 // Ctrl + Shift+ / : 선택 범위 주석 처리/해제 (/* */)
+    }
+}
