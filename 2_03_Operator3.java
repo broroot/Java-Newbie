b6f7682e3665b8440df8aa00f8d61436d56659d4
broroot <168093278+broroot@users.noreply.github.com>
2025-07-23 23:14:38 +0900

    03 비교 연산자
    >
    >=
    ==
    !=
    (파이썬과 동일)

---
+++ 2_03_Operator3.java
@@ -0,0 +1,11 @@
+package Chap_02;
+
+public class _03_Operator3 {
+    public static void main(String[] args) {
+        // 비교 연산자 - 파이썬이랑 같음
+        System.out.println(5 > 3);
+        System.out.println(5 >= 3);
+        System.out.println(5 == 3);
+        System.out.println(5 != 3); 
+    }
+}
