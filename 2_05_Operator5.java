9bba508358478f117c17f003b1aa42a4e33c7a2c
broroot <168093278+broroot@users.noreply.github.com>
2025-07-23 23:16:01 +0900

    05 삼항 연산자
    결과  = (조건) ? (참 결과값) : (거짓 결과값);

---
+++ 2_05_Operator5.java
@@ -0,0 +1,12 @@
+package Chap_02;
+
+public class _05_Operator5 {
+    public static void main(String[] args) {
+        // 삼항 연산자
+        // 결과  = 조건 ? 참 결과값 : 거짓 결과값
+        int x=3;
+        int y=5;
+        int max = (x>y) ? x : y;
+        System.out.println(max);
+    }
+}
