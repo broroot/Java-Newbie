87c23777395d37f17765dcdf9713b2f2b8d4980c
broroot <168093278+broroot@users.noreply.github.com>
2025-07-24 23:25:11 +0900

    Add files via upload

---
+++ _Quiz_03.java
@@ -0,0 +1,9 @@
+package Chap_03;
+
+public class _Quiz_03 {
+    public static void main(String[] args) {
+        String s = "901231-1234567";
+        System.out.println(s.substring(0,8));
+        System.out.println(s.substring(0,s.indexOf("-")+2));
+    }
+}
